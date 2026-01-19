8f7cf487a3637282f3b73ac28436f78e6ea9eaf4
Michiel <Michielnooij@icloud.com>
2026-01-19 20:20:21 +0100

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Michiel Nooij on 19/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
